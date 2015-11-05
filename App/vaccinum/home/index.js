bf6fc1c72f04b5ef165056da821453f0747ab10a
fov42550564 <42550564@qq.com>
2015-11-05 16:44:37 +0800

    finish vaccinum home page

--- App/vaccinum/home/index.js
+++ App/vaccinum/home/index.js
@@ -7,17 +7,31 @@ var {
     View,
     Text,
     Image,
-    TouchableHighlight,
+    TouchableOpacity,
 } = React;
 
+
+var CCTouchable =  React.createClass({
+    render() {
+        return (
+            <TouchableOpacity
+                onPress={this.props.onPress}
+                activeOpacity={0.2}
+                style={this.props.style}
+                >
+                {this.props.children}
+            </TouchableOpacity>
+        )
+    }
+});
 
 module.exports = React.createClass({
     getInitialState() {
         return {
-            username: '',
-            birthday: '',
-            nextVaccinumTime: '',
-            nextVaccinumName: ''
+            username:'方远航',
+            birthday:'3岁1个月15天',
+            nextVaccinumTime:'2017-01-09',
+            nextVaccinumName:'百白破'
         };
     },
     enterPersonal(){
@@ -29,51 +43,139 @@ module.exports = React.createClass({
     render() {
         return (
             <View style={styles.container}>
-                <TouchableHighlight
+                <CCTouchable
                     onPress={this.enterPersonal}
-                    underlayColor="#EEB422">
-                    <View style={styles.personalContainer}>
+                    style={styles.personalContainer}
+                    >
+                    <View style={styles.headContainer}>
+                        <Image
+                            resizeMode='stretch'
+                            source={require('../image/personalHead.jpg')}
+                            style={styles.head} />
                     </View>
-                </TouchableHighlight>
+                    <View style={styles.infoContainer}>
+                        <View style={styles.nameContainer}>
+                            <Text style={styles.username}>
+                                {this.state.username}
+                            </Text>
+                            <Text style={styles.birthday}>
+                                {this.state.birthday}
+                            </Text>
+                        </View>
+                        <View style={styles.nextVaccinumTimeContainer}>
+                            <Text style={styles.label}>下次接种时间:</Text>
+                            <Text style={styles.nextVaccinumTime}>
+                                {this.state.nextVaccinumTime}
+                            </Text>
+                        </View>
+                        <View style={styles.nextVaccinumNameContainer}>
+                            <Text style={styles.label}>疫苗名称:</Text>
+                            <Text style={styles.nextVaccinumName}>
+                                {this.state.nextVaccinumName}
+                            </Text>
+                        </View>
+                    </View>
+                </CCTouchable>
 
-
-                <TouchableHighlight
+                <CCTouchable
                     onPress={this.enterVaccinumSearch}
-                    underlayColor="#EEB422">
-                    <View style={styles.vaccinumSearchContainer}>
-                    </View>
-                </TouchableHighlight>
+                    style={styles.vaccinumSearchContainer}
+                    >
+                    <Image
+                        resizeMode='contain'
+                        source={require('../image/vaccinumSearch.png')}
+                        style={styles.menuImage} />
+                    <Text style={styles.menuText}>接种疫苗查询</Text>
+                </CCTouchable>
 
-
-                <TouchableHighlight
+                <CCTouchable
                     onPress={this.enterVaccinumBaike}
-                    underlayColor="#EEB422">
-                    <View style={styles.vaccinumBaikeContainer}>
-                    </View>
-                </TouchableHighlight>
+                    style={styles.vaccinumBaikeContainer}
+                    >
+                    <Image
+                        resizeMode='contain'
+                        source={require('../image/vaccinumBaike.jpg')}
+                        style={styles.menuImage} />
+                    <Text style={styles.menuText}>疫苗百科</Text>
+                </CCTouchable>
             </View>
         );
     }
 });
 
 var styles = StyleSheet.create({
-    container: {
-        flex: 1,
+    container:{
+        flex:1,
     },
-    personalContainer: {
-        flex: 2,
-        alignItems: 'center',
-        justifyContent: 'center'
+    personalContainer:{
+        flex:2,
+        backgroundColor:'#D8BFD8',
     },
-    nextVaccinumSearchContainer: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        background
+    vaccinumSearchContainer:{
+        flex:1,
+        borderTopWidth:2,
+        justifyContent:'center',
+        alignItems:'center',
     },
-    vaccinumSearchContainer: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
+    vaccinumBaikeContainer:{
+        flex:1,
+        borderTopWidth:2,
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    //顶部栏
+    headContainer:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    infoContainer:{
+        height:120,
+        justifyContent:'space-around',
+        alignItems:'center',
+    },
+    head:{
+        width:150,
+        height:150,
+        borderRadius:75,
+    },
+    nameContainer:{
+        flexDirection:'row'
+    },
+    username:{
+        fontSize:24,
+        color:'#CD6839',
+        marginRight:20,
+    },
+    birthday:{
+        fontSize:14,
+        color:'#FF6347',
+        alignSelf:'center',
+    },
+    nextVaccinumTimeContainer:{
+        flexDirection:'row'
+    },
+    label:{
+        fontSize:14,
+        marginRight:20,
+    },
+    nextVaccinumTime:{
+        fontSize:18,
+        color:'red',
+    },
+    nextVaccinumNameContainer:{
+        flexDirection:'row'
+    },
+    nextVaccinumName:{
+        fontSize:18,
+        color:'red',
+    },
+    //菜单栏
+    menuImage:{
+        flex:1,
+    },
+    menuText:{
+        fontSize:14,
+        margin:20,
     },
 });
